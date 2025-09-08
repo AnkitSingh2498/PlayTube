d2dcb337c9f58d8b115bef5848d0ad7e6eb38a72
AnkitSingh2498 <ankit.singh.241198@gmail.com>
2025-09-08 20:45:28 +0530

    Utils code (Error Handling, Response)

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const DB_NAME = "playtube"
+export const DB_NAME = "playtube";
+export const LIMIT = "16kb";
--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+app.listen(process.env.PORT || 8000, ()=>{
+  console.log(`Server is running at PORT: ${process.env.PORT}`);
+})
 
+})
+.catch((err)=>{
+  console.log("MongoDB connection Failed", err);
+  
+})
 
 
 
